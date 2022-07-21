8d83f5d17c426631c63e1cc89e78c222a361d2d6
王春 <itchun@163.com>
2022-07-21 09:58:50 +0800

    提交

---
+++ src/main/java/com/base/util/MainUtil.java
@@ -0,0 +1,11 @@
+package com.base.util;
+
+public class MainUtil {
+
+    public static void main(String[] args) {
+        int i = 0;
+        double e = i;
+        double r = 9D;
+        int w = (int)r;
+    }
+}
